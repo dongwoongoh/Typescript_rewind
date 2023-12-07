606c3066cb24874ce2750923372d14d5b53df2ce
maaaaaaaaad <integral5137@gmail.com>
2023-12-07 21:04:27 +0900

    e

--- iteration_create_books.sh
+++ iteration_create_books.sh
@@ -4,4 +4,8 @@ for i in {1..10}; do
     title="Test Book$i"
     author="Test Author"
     curl -X POST -H "Content-Type: application/json" -d "{\"title\": \"$title\", \"author\": \"$author\"}" http://localhost:8080/books
+    if [ $? -ne 0 ]; then
+        echo "error: need to check the server status"
+        exit 1
+    fi
 done
