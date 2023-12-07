73b6e2d43f81167542ed650fe089fe343f6c9f54
maaaaaaaaad <integral5137@gmail.com>
2023-12-07 17:15:25 +0900

    e

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=localhost user=myuser password=mypassword dbname=library_db port=5432 sslmode=disable TimeZone=Asia/Seoul"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println(db)
+
+}
