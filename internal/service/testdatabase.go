f77571535b3c94397b8fddfab99a7e2bcb673f42
maaaaaaaaad <integral5137@gmail.com>
2023-12-09 17:27:57 +0900

    e

--- internal/service/testdatabase.go
+++ internal/service/testdatabase.go
@@ -12,7 +12,7 @@ func SetupDatabase(t *testing.T) *gorm.DB {
 	if err != nil {
 		t.Fatalf("Failed to connect to database: %v", err)
 	}
-	if err := db.AutoMigrate(&model.Book{}); err != nil {
+	if err := db.AutoMigrate(&model.Book{}, &model.User{}, &model.LoanRecord{}); err != nil {
 		t.Fatalf("Failed to migrate database: %v", err)
 	}
 	return db
--- internal/service/userservice_test.go
+++ internal/service/userservice_test.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"testing"
+	"web_application/internal/model"
+)
+
+func TestCreateUser(t *testing.T) {
+	db := SetupDatabase(t)
+	userService := NewUserService(db)
+	user := &model.User{Name: "Mad", Email: "integral"}
+	err := userService.CreateUser(user)
+	if err != nil {
+		t.Errorf("Failed to create user: %v", err)
+	}
+}
+
+func TestCreateUserFail(t *testing.T) {
+	db := SetupDatabase(t)
+	userService := NewUserService(db)
+	invalidUser := &model.User{Name: "MAD", Email: ""}
+	err := userService.CreateUser(invalidUser)
+	if err.Error() != "field empty" {
+		t.Errorf("message %v", err)
+	}
+}
